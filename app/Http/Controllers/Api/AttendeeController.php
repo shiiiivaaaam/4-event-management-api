c969d0b9c4b578bc31ba895d2401c4d719a1794d
shivam <shivamkumar.official55@gmail.com>
2025-10-31 14:32:21 +0530

    final commit

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -16,6 +16,8 @@ class AttendeeController extends Controller
 
     public function __construct(){
         $this->middleware('auth:sanctum')->except(['index','update']);
+        $this->middleware('throttle:api')
+        ->only(['store','update','destroy']);
         $this->authorizeResource(Attendee::class,'attendee');
     }
 
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,7 +24,10 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->configureRateLimiting();
+        // $this->configureRateLimiting();
+        RateLimiter::for('api',function(Request $request){
+            return Limit::perMinute(60)->by($request->user()?->id ?:$request->ip());
+        });
 
         $this->routes(function () {
             Route::middleware('api')
